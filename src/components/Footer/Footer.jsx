7ba52fb925ae5d6fee4900ee61a758e3553445c1
JakeR-Dev <jryan6492@gmail.com>
2026-01-02 11:05:25 -0500

    footer copyright formatting

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       {/* <img src="/PowderDay/powder-day-logo.svg" alt="Powder Day Logo" className="footer-logo" /> */}
-      <p>Powder Day &copy; 2026</p>
+      <p>[ Powder Day - 2026 ]</p>
       <a href="https://github.com/JakeR-Dev/PowderDay" aria-label="visit the github repo"><img src="/PowderDay/github-white.png" alt="GitHub Logo" /></a>
       <a href="https://www.linkedin.com/in/jakepotterryan/" aria-label="visit my linkedin profile"><img src="/PowderDay/linkedin-white.png" alt="LinkedIn Logo" /></a>
     </footer>
