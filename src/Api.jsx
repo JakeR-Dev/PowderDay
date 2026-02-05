a41202f9e0cdf45c36f58a2ad7bee871a2766173
JakeR-Dev <jryan6492@gmail.com>
2026-02-05 16:01:24 -0500

    start adding weather

--- src/Api.jsx
+++ src/Api.jsx
@@ -49,3 +49,40 @@ export async function getResortSnowReport(resortId) {
     console.error(error);
   }
 }
+
+// get resort coordinates name
+export async function getResortCoordinates(resortName, resortState) {
+  const resortStr = resortName + ',' + resortState;
+  const url = 'https://nominatim.openstreetmap.org/search?q=' + resortStr + '&format=json&limit=1&countrycodes=CA,US';
+  const options = {
+    method: 'GET'
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = JSON.parse(await response.text());
+    const coordinates = result ? result[0].lat + ',' + result[0].lon : 'null';
+    return coordinates;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// getResortZip('Okemo Mountain Resort', 'VT');
+
+// get weather info for resort
+export async function getResortWeather(resortName, resortState) {
+  const coordinates = await getResortCoordinates(resortName, resortState);
+  const url = 'https://api.weatherapi.com/v1/current.json?key=7af5f47e0fc24880a32195158260502&q=' + coordinates;
+  const options = {
+    method: 'GET'
+  };
+
+  try {
+    const response = await fetch(url, options);
+    const result = await response.text();
+    return JSON.parse(result);
+  } catch (error) {
+    console.error(error);
+  }
+}
